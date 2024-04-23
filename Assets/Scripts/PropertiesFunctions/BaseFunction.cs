4889f005199a4580e0799b3fb3dbb898e0ffacdc
DJVince81 <39565195+DJVince81@users.noreply.github.com>
2024-04-23 12:04:18 +0200

    dev : Rework first implementation Unity WebGL & Interface

--- Assets/Scripts/PropertiesFunctions/BaseFunction.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BaseFunction : MonoBehaviour
-{
-    internal abstract void Execute(List<string> args);
-    internal abstract void Execute(List<object> args);
-}
