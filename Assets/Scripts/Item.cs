26507d5879a6480c1b52125dee89d8a2c6f141c5
Poulpefaible <60007891+Poulpefaible@users.noreply.github.com>
2024-04-26 10:53:02 +0200

    shop that works

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Item : MonoBehaviour
 {
     public int price;
+    private bool placed = false;
     public TextMeshProUGUI priceText;
+    public GameObject image;
 
     void Start()
     {
@@ -15,6 +18,11 @@ public class Item : MonoBehaviour
 
     public void pay(Shop shop)
     {
-        if(shop.getMoney() >= price) shop.changeMoney(-price);
+        if (shop.getMoney() >= price && !placed)
+        {
+            shop.changeMoney(-price);
+            image.SetActive(true);
+            placed = true;
+        }
     }
 }
