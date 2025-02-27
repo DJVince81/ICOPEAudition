aedc07308922d0902b1123e9e102092a2b8f5329
Floshapo <60699084+Floshapo@users.noreply.github.com>
2025-02-27 12:02:24 +0100

    Documentation UITutorialControler.cs
    Ajoute de la documentation pour toutes les méthodes public et priver.

--- Assets/Scripts/UI/TutorialContents/UITutorialControler.cs
+++ Assets/Scripts/UI/TutorialContents/UITutorialControler.cs
@@ -2,15 +2,35 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
+/// <summary>
+/// Controler that manage text placement for the Tutorial.
+/// </summary>
 public class UITutorialControler : MonoBehaviour
 {
+    #region SerializedField TMP_text
     [SerializeField] private TMP_Text _intituleText;
     [SerializeField] private TMP_Text _Text;
+    #endregion
 
+    #region Local variable
     private XMLReader xmlReader;
     private int indexText = 0;
     private string nameStep;
+    #endregion
 
+    #region Public methods
+    /// <summary>
+    /// Check the currentState of the game and load text for tutorial.
+    /// </summary>
+    /// <remarks>
+    /// Here a list of actual gameState and it's links name for seaching the text attach.
+    /// GAME_MENU -> Waiting_room,
+    /// GAME_E0 -> Step_0,
+    /// GAME_E1 -> Step_1,
+    /// GAME_E2 -> Step_2,
+    /// GAME_E3 -> Step_3,
+    /// GAME_E4 -> Step_4
+    /// </remarks>
     public void checkStateTutorial()
     {
         switch (GameManager.Instance.StatesManager.currentState)
@@ -22,12 +42,25 @@ public class UITutorialControler : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Change indexText value for loading next text.
+    /// </summary>
     public void nextTextButton()
     {
         indexText++;
         setTexts(nameStep, indexText);
     }
+    #endregion
 
+    #region Private methods
+    /// <summary>
+    /// Set text with the xml information.
+    /// </summary>
+    /// <remarks>
+    /// Set text for an intitule and Text (dialogue).
+    /// </remarks>
+    /// <param name="nameStep">The name of the current state for the XML file.</param>
+    /// <param name="idSteps">The ids of the current text to load.</param>
     private void setTexts(string nameStep, int idSteps)
     {
         Entry entry = xmlReader.readXmlStream(nameStep, idSteps);
@@ -43,6 +76,9 @@ public class UITutorialControler : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Check if input button like mouse click (Fire1) and keyboard (enter) is hit and call fuction "nextTextButton".
+    /// </summary>
     private void getInputs()
     {
         if (Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Return))
@@ -50,7 +86,9 @@ public class UITutorialControler : MonoBehaviour
             nextTextButton();
         }
     }
+    #endregion
 
+    #region Unity method
     private void Awake()
     {
         xmlReader = GetComponent<XMLReader>();
@@ -64,5 +102,6 @@ public class UITutorialControler : MonoBehaviour
     private void Update()
     {
         if (GameManager.Instance._tutorialPanel.activeSelf) getInputs();
-    }  
+    }
+    #endregion
 }
